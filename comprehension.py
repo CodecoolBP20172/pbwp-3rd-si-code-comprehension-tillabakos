5bf24fd8655570386d5368acbd5d7a2791ad9a63
tilla <tilla.bakos@gmail.com>
2017-07-09 09:22:49 +0200

    commented

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,35 @@
 import random
 
-guessesTaken = 0
+#  Geassing game. Runs until input maches the number to be guessed or gesses reach max guesses defined.    
 
-print('Hello! What is your name?')
-myName = input()
+guessesTaken = 0  # value assigned to variable
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print('Hello! What is your name?')  # print string to console
+myName = input()  # assigne consol input to variable
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+number = random.randint(1, 20)  # assigne value to variable randomely from range 1 to 20
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # print string and value of variable to console
 
-    guessesTaken += 1
+while guessesTaken < 6:  # loop until condition is True
+    print('Take a guess.')  # print string to console
+    guess = input()  # assigne console input to variable
+    guess = int(guess)  # change type of variable to integer
 
-    if guess < number:
-        print('Your guess is too low.')
+    guessesTaken += 1  # add one to variable in every loop
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess < number:  # check if condition is True
+        print('Your guess is too low.')  # print string to console
 
-    if guess == number:
-        break
+    if guess > number:  # check if condition is True
+        print('Your guess is too high.')  # print string to console
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess == number:  # check if condition is True
+        break  # stop the loop an d go to the next line
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess == number:  # check if condition is True
+    guessesTaken = str(guessesTaken)  # change the type of varianble to string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # print strings combined with variables to console
+
+if guess != number:  # check if condition is True
+    number = str(number)  # change the type of varianble to string
+    print('Nope. The number I was thinking of was ' + number)  # print string and variable to console
